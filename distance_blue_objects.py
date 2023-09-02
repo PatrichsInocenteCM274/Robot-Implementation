d5e62e9e4f118fd4a57be2038254aa255314a130
Patrichs Inocente <30361234+PatrichsInocenteCM274@users.noreply.github.com>
2023-09-02 10:18:47 -0500

    Update distance_blue_objects.py

--- distance_blue_objects.py
+++ distance_blue_objects.py
@@ -20,21 +20,15 @@ def dibujar(mask,color):
       cv2.putText(frame,'{},{}'.format(x[indice],y[indice]),(x[indice]+10,y[indice]), font, 0.75,(0,255,0),1,cv2.LINE_AA)
       cv2.drawContours(frame, [nuevoContorno], 0, color, 3)
       indice = indice+1
-  cv2.putText(frame,'Distance: {}'.format(math.sqrt(pow(x[1]-x[0],2)+pow(y[1]-y[0],2))),(0,20), font, 0.75,(0,255,0),1,cv2.LINE_AA)
+  if indice ==1:
+      cv2.putText(frame,'Distance: {}'.format(0.0),(0,20), font, 0.75,(0,255,0),1,cv2.LINE_AA)
+  else:
+      cv2.putText(frame,'Distance: {}'.format(math.sqrt(pow(x[1]-x[0],2)+pow(y[1]-y[0],2))),(0,20), font, 0.75,(0,255,0),1,cv2.LINE_AA)
 
-cap = cv2.VideoCapture('http://192.168.1.33:8080/video')
+cap = cv2.VideoCapture('http://192.168.43.1:8080/video')
 
 azulBajo = np.array([100,100,20],np.uint8)
 azulAlto = np.array([125,255,255],np.uint8)
-
-amarilloBajo = np.array([15,100,20],np.uint8)
-amarilloAlto = np.array([45,255,255],np.uint8)
-
-redBajo1 = np.array([0,100,20],np.uint8)
-redAlto1 = np.array([5,255,255],np.uint8)
-
-redBajo2 = np.array([175,100,20],np.uint8)
-redAlto2 = np.array([179,255,255],np.uint8)
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 while True:
@@ -44,13 +38,7 @@ while True:
   if ret == True:
     frameHSV = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
     maskAzul = cv2.inRange(frameHSV,azulBajo,azulAlto)
-    maskAmarillo = cv2.inRange(frameHSV,amarilloBajo,amarilloAlto)
-    #maskRed1 = cv2.inRange(frameHSV,redBajo1,redAlto1)
-    #maskRed2 = cv2.inRange(frameHSV,redBajo2,redAlto2)
-    #maskRed = cv2.add(maskRed1,maskRed2)
     dibujar(maskAzul,(255,0,0))
-    #dibujar(maskAmarillo,(0,255,255))
-    #dibujar(maskRed,(0,0,255))
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('s'):
       break
